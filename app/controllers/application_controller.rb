a78050e9168f51032ff1bee2d7d14750e4f43b98
tomoR2 <144089090+tomoR2@users.noreply.github.com>
2023-10-23 17:50:57 +0900

    ログイン、ログアウト、ボタン切替の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,15 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :birth_day])
   end
+
+  # deviseのデフォルトはroot_pathなのでpath変更
+  protected
+
+  def after_sign_in_path_for(resource)
+    users_index_path
+  end
+
+  def after_sign_out_path_for(resource)
+    users_index_path
+  end
 end
