b4e94bf38ed2c49bc14eec38f565a3606a5ab365
tomoR2 <144089090+tomoR2@users.noreply.github.com>
2023-10-25 17:37:24 +0900

    管理者権限の作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_22_104405) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_25_082654) do
   create_table "messages", charset: "utf8mb4", force: :cascade do |t|
     t.text "message_text"
     t.bigint "user_id", null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_22_104405) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
